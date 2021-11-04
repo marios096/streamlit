1d42180ff572328c9bd4e7106aca2eb2aa8f2c74
Marios <46581330+marios096@users.noreply.github.com>
2021-11-04 18:57:51 +0200

    Update existing_data.py

--- existing_data.py
+++ existing_data.py
@@ -21,7 +21,7 @@ def streamlit_app():
 
     st.sidebar.title("🛈 About")
     st.sidebar.info('Created and maintained by:' + '\r' + '[Giorgos Savva](xristofo@gmail.com)')
-    st.sidebar.text('[Antonis Savvidis] \n[Marios Charalambous]')
+    st.sidebar.text_area('[Antonis Savvidis] \n[Marios Charalambous]')
 
     with st.spinner(text='Loading Data! Please wait...'):
         cyprus_vac_df = load_data_vac()
