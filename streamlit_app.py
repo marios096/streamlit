9af75d0790beb8b2dbc786c02bc58dd8d4f79f6f
giorgossavva <george16savva@gmail.com>
2021-11-04 11:34:28 +0200

    sex

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ run the app by installing streamlit with pip and typing
 """
 
 import streamlit as st
+import existing_data
 import pandas as pd
 #import matplotlib.pyplot as plt
 import numpy as np
@@ -14,7 +15,7 @@ import numpy as np
 import datetime as dt
 
 # Read in the dataset
-airbnb = pd.read_csv('https://github.com/marios096/streamlit/blob/main/data.csv?raw=true')
+#i will put to comments this airbnb = pd.read_csv('https://github.com/marios096/streamlit/blob/main/data.csv?raw=true')
 
 #st.title('Simple Streamlit App')
 
@@ -25,8 +26,31 @@ airbnb = pd.read_csv('https://github.com/marios096/streamlit/blob/main/data.csv?
 #st.write(f'{n} + 1 = {n+1}')
 #
 #s = st.text_input('Type a name in the box below')
-#newdf1 = airbnb.groupby(['Suburb', 'Address'])
-  
-# print new dataframe
-#print(newdf1)
-st.write(airbnb)
+
+#i will put to comments this st.write(f'Hello {airbnb.head()}')
+
+#trying to clear the duplicates 19_10_2021
+#drop duplicates from suburb, address and date step 1
+#airbnb=airbnb.drop_duplicates(subset=['Suburb','Address','Date','Price','Rooms','Type','Method','SellerG','Regionname','Postcode','Propertycount','Distance','CouncilArea'], keep='last')
+#step 2
+#airbnb=airbnb.drop_duplicates(subset=['Suburb','Address','Date','Price'], keep='last')
+#airbnb.loc[airbnb.duplicated(subset=['Suburb','Address','Date','Price']),:]
+#temp=airbnb[airbnb.duplicated(subset=['Suburb','Address','Date'])]
+#temp
+#i will put to comments this airbnb.sort_values(by=['Price'], inplace=True)
+#i will put to comments this df = airbnb[airbnb[(['Suburb','Address','Date'])].duplicated(keep=False)]
+#df.index
+
+#the drop must be done in airbnb to change the rows of csv
+#i will put to comments this df=df.dropna(subset=['Price'])
+#i will put to comments this df
+
+PAGES = {
+    "About us": existing_data
+}
+
+st.set_page_config(page_title="Covid19-Cyprus", page_icon="🧊", layout='wide', initial_sidebar_state='auto')
+st.sidebar.title('🧭 Navigation')
+selection = st.sidebar.radio("", list(PAGES.keys()))
+page = PAGES[selection]
+page.streamlit_app()
